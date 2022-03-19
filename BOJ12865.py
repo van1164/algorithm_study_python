2bb13ecab30b5d43a68f42ed4a1af76125dd853c
van133 <vkn123@naver.com>
2022-03-19 22:54:05 +0900

    BOJ12865
    사용 알고리즘 : 다이나믹 프로그래밍
    dp를 업데이트 하는 과정에서 그후에 업데이트에도 영항을 미치기 때문에 영향을 끼치지 않게 미리 복사해놓은 후에 업데이트 함.

--- BOJ12865.py
+++ BOJ12865.py
@@ -3,19 +3,17 @@ dp = {0:0}
 lst = []
 for i in range(a):
     lst.append(list(map(int,input().split())))
-lst = sorted(lst,key=lambda x:-x[0])
+lst = sorted(lst,key=lambda x:x[0])
 for i in lst:
     if i[0] >b:
         break
-    for j in list(dp.keys()):
+    for j,k in list(dp.items()):
         if j + i[0]>b:
             continue
         else:
             if j +i[0] not in dp:
-                dp[j+i[0]] = dp[j]+i[1]
+                dp[j+i[0]] = k+i[1]
             else:
-                if dp[j+i[0]] < dp[j]+i[1]:
-                    dp[j+i[0]] = dp[j]+i[1]
-                    
-                    
+                if dp[j+i[0]] < k+i[1]:
+                    dp[j+i[0]] = k+i[1]                 
 print(max(dp.values()))
